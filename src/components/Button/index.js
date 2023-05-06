21a1ef83d06c63e02647abde70adff8c7f2b57f5
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 16:38:58 -0300

    Update index.js

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import { ButtonContainer } from './styles';
 
-function Button({onClick}) {
-  return (
-    <ButtonContainer onClick={onClick}>
-       Buscar
-    </ButtonContainer>
-  )
-}
+const Button = ({ onClick }) => {
+return (
+<ButtonContainer onClick={onClick}>
+Buscar
+</ButtonContainer>
+);
+};
 
-export default Button
+Button.propTypes = {
+onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
