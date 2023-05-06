f3b288cea3bbe7248c6cb6424572cdb71c5a09ff
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 16:43:21 -0300

    Update styles.js

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,24 +1,19 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-export const InputContainer = styled.div`
+const InputContainer = styled.div border: 1px solid #fafafa; border-radius: 20px; height: 62px; width: 80%; margin: 20px; input { background: transparent; border: 0; width: 90%; height: 62px; padding: 0 20px; color: #ffffff; font-size: 20px; };
 
-    border: 1px solid #FAFAFA;
-    border-radius: 20px;
+const Input = ({ value, onChange }) => {
+return (
+<InputContainer>
+<input value={value} onChange={onChange} />
+</InputContainer>
+);
+};
 
-    height: 62px;
-    width: 80%;
+Input.propTypes = {
+value: PropTypes.string.isRequired,
+onChange: PropTypes.func.isRequired,
+};
 
-    margin: 20px;
-    
-
-    input {
-        background: transparent;
-        border: 0;
-        width: 90%;
-        height: 62px;
-        padding: 0 20px;
-        color: #FFFFFF;
-        font-size: 20px
-    }
-
-`
+export default Input;
